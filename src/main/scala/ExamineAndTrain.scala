3bde5205c020519e2b92a9da1d8b5d4f5f05d363
Massil <massil.achab@gmail.com>
2016-04-24 15:20:49 +0200

    new files

---
+++ src/main/scala/ExamineAndTrain.scala
@@ -0,0 +1,4 @@
+import com.google.gson.{GsonBuilder, JsonParser}
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
